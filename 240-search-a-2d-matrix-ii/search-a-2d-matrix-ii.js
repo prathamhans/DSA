ea114aa6e388ed9dc17b773f24b75f9cadb12258
Pratham Hans <145926804+prathamhans@users.noreply.github.com>
2026-08-11 22:19:25 +0530

    Time: 1182 ms (13.32%) | Memory: 59.7 MB (9.11%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.js
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.js
@@ -1,18 +1,15 @@
-/**
- * @param {number[][]} matrix
- * @param {number} target
- * @return {boolean}
- */
 var searchMatrix = function(arr, target) {
     let row=0
-    let col = arr[0].length -1
+    let col= arr[0].length-1
     while(row<arr.length && col>=0){
-        if(arr[row][col] == target) return true
-        if(arr[row][col]<target){
-            row++
-        }else{
+        if(target == arr[row][col]) return true;
+
+        if(target < arr[row][col]){
             col--
+        } 
+        if(target > arr[row][col]){
+            row++
         }
     }
-return false
+    return false
 };
