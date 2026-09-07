c800494b86197eaa31733c4c96d15481c13043ae
Pratham Hans <145926804+prathamhans@users.noreply.github.com>
2026-09-08 00:23:03 +0530

    Time: 0 ms (100.00%) | Memory: 57.9 MB (36.12%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.js
+++ 206-reverse-linked-list/reverse-linked-list.js
@@ -1,21 +1,49 @@
 
-var reverseList = function(head) {
-    if(head == null) return null
-   let curr= head
-   let stack=[]
-   while(curr!= null){
-    stack.push(curr)
-    curr= curr.next
-   } 
-   let newhead = stack.pop()
-   let start= newhead
-   while(stack.length>0){
-    let nextnode = stack.pop()
-    newhead.next= nextnode
-    newhead = nextnode
-   }
+// var reverseList = function(head) {
+//     if(head == null) return null
+//    let curr= head
+//    let stack=[]
+//    while(curr!= null){
+//     stack.push(curr)
+//     curr= curr.next
+//    } 
+//    let newhead = stack.pop()
+//    let start= newhead
+//    while(stack.length>0){
+//     let nextnode = stack.pop()
+//     newhead.next= nextnode
+//     newhead = nextnode
+//    }
 
-    newhead.next = null
-   return start
+//     newhead.next = null
+//    return start
+   
+// };
+
+
+
+
+
+var reverseList = function(head) {
+    let previous =  null
+    let current  = head
+    while(current!=null){
+        let storedata= current.next 
+        current.next =previous
+        previous= current 
+        current = storedata 
+    }
+
+    return  previous
    
 };
+
+
+
+
+
+
+
+
+
+
