f49d23adf03a4795d24d6afa4d2f4656ebf04071
Pratham Hans <145926804+prathamhans@users.noreply.github.com>
2026-09-18 00:07:56 +0530

    Time: 5 ms (53.59%) | Memory: 79.4 MB (43.26%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.js
+++ 234-palindrome-linked-list/palindrome-linked-list.js
@@ -1,45 +1,26 @@
-// var isPalindrome = function(head) {
-//     let curr= head
-//     let stack=[]
-//     while(curr!=null){
-//         stack.push(curr.val)
-//         curr= curr.next
-//     }
-//     curr= head
-//     while(curr!=null){
-//          if(curr.val!=stack[stack.length-1]){
-//             return false
-//          }
-//         curr= curr.next
-//         stack.pop()
-//     }
-   
-//     return true
-// };
 var isPalindrome = function(head) {
-    let slow=head
-    let fast = head
-   while(fast.next!=null && fast.next.next!=null ){
-    slow=slow.next
+let slow= head
+let fast= head
+while(fast.next!=null  && fast.next.next!=null){
+    slow= slow.next
     fast = fast.next.next
-   }
-   
-   let secondhead= slow.next
-   let current=  secondhead
-   let prev=null
-   while(current!=null){
-    let store = current.next
-    current.next= prev
-    prev= current
-    current= store
-   }
-   let curr=head
-  while(prev != null && curr!=null){
-     if(prev.val !=curr.val){
-    return false
-   }
-    curr=curr.next
+}
+let newheadforsecondll= slow.next
+let curr= newheadforsecondll
+let prev= null
+while(curr!=null){
+    let store= curr.next
+    curr.next = prev
+    prev = curr
+    curr= store
+}
+let oldcurrent= head
+while(prev!=null  && oldcurrent!=null ){
+    if(prev.val !=  oldcurrent.val){
+        return false 
+    }
     prev= prev.next
-  }
-  return true
+    oldcurrent= oldcurrent.next
+}
+return true
 };
